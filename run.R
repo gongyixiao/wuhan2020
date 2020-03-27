8c26696a3a3b2376dbd1e57687c5fe7b85361a4f
gongyixiao <gongyixiao@gmail.com>
2020-03-27 12:05:57 -0400

    v1

--- run.R
+++ run.R
@@ -8,12 +8,18 @@ args = commandArgs(trailingOnly=TRUE)
 
 fread(args[1]) %>% 
   replace(., is.na(.), "0") %>%
-  mutate(City = {if("Admin2" %in% names(.)) Admin2 else "City"}) %>%
-  select(`City`,`Province_State`,`Country_Region`,`Last_Update`,`Confirmed`, `Deaths`,`Recovered`) %>%
-  mutate(`Province_State` = ifelse(`Province_State`=="", `Country_Region`, `Province_State`)) %>%
-  mutate(`City` = ifelse(`City`=="City", `Province_State`, `City`)) %>%
-  mutate(`Last_Update` = strsplit(`Last_Update`," |T")[[1]][1]) %>%
-  rename(`Date`=`Last_Update`) %>%
+  mutate(Admin2 = {if("Admin2" %in% names(.)) Admin2 else ""}) %>%
+  mutate(`Province/State` = {if("Province_State" %in% names(.)) Province_State else `Province/State`}) %>%
+  mutate(`Country/Region` = {if("Country_Region" %in% names(.)) Country_Region else `Country/Region`}) %>%
+  mutate(`Last Update` = {if("Last_Update" %in% names(.)) Last_Update else `Last Update`}) %>%
+  rename(`Date`=`Last Update`) %>%
+  mutate(`Province/State` = ifelse(grepl(",", `Province/State`), gsub(", ", ",",`Province/State`), paste0("City,",`Province/State`))) %>%
+  separate(`Province/State`, into = c("City", "Province/State"), sep=",") %>%
+  mutate(`Province/State` = ifelse(`Province/State`=="", `Country/Region`, `Province/State`)) %>%
+  mutate(`Province/State` = ifelse(is.na(state.abb[match(`Province/State`, state.name)]),`Province/State`, state.abb[match(`Province/State`, state.name)])) %>%
+  mutate(`City` = ifelse(Admin2=="", `Province/State`, Admin2)) %>%
+  mutate(City = ifelse((`Country/Region`=="US")&(`City`!=`Province/State`), paste(City, `Province/State`,sep="/"), City)) %>%
+  mutate(`Date` = strsplit(`Date`," |T")[[1]][1]) %>%
+  mutate(`Date` = ifelse(grepl("/", `Date`),paste(paste0("20",strsplit(`Date`,"/")[[1]][3]),strsplit(`Date`,"/")[[1]][1],strsplit(`Date`,"/")[[1]][2],sep="-"),`Date`)) %>% View()
+  select(`City`,`Province/State`,`Country/Region`,`Date`,`Confirmed`, `Deaths`,`Recovered`) %>%
   fwrite(file = "data.csv", append = TRUE, quote = FALSE, sep = ",", col.names = FALSE)
-
-
--- run.bash
+++ run.bash
@@ -4,7 +4,4 @@ if [[ ! -f data.csv ]]
 then
 	echo "City,Province/State,Country/Region,Date,Confirmed,Deaths,Recovered" > data.csv
 fi
-tmp=`mktemp`
-cat $1 | sed 's/\//_/g' | sed 's/Last Update/Last_Update/g' > $tmp
-Rscript ./run.R $tmp
-rm -rf $tmp
+Rscript ./run.R $1
