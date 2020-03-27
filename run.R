217c1c9b701df91cdc55a755d475a1a728685c18
gongyixiao <gongyixiao@gmail.com>
2020-03-27 12:32:57 -0400

    change orders of command

--- run.R
+++ run.R
@@ -10,15 +10,15 @@ fread(args[1]) %>%
   replace(., is.na(.), "0") %>%
   mutate(Admin2 = {if("Admin2" %in% names(.)) Admin2 else ""}) %>%
   mutate(`Province/State` = {if("Province_State" %in% names(.)) Province_State else `Province/State`}) %>%
+  mutate(`Country/Region` = {if("Country_Region" %in% names(.)) Country_Region else `Country/Region`}) %>%
+  mutate(`Last Update` = {if("Last_Update" %in% names(.)) Last_Update else `Last Update`}) %>%
+  rename(`Date`=`Last Update`) %>%
   filter(`Country/Region` != "The Bahamas") %>%
   mutate(`Country/Region` = gsub("Bahamas, The","Bahamas", `Country/Region`)) %>%
   filter(`Country/Region` != "The Gambia") %>%
   mutate(`Country/Region` = gsub("Gambia, The","Gambia", `Country/Region`)) %>%
   mutate(`Country/Region` = gsub("Korea, South","South Korea", `Country/Region`)) %>%
   mutate(`Country/Region` = gsub("Republic of Korea","South Korea", `Country/Region`)) %>%
-  mutate(`Country/Region` = {if("Country_Region" %in% names(.)) Country_Region else `Country/Region`}) %>%
-  mutate(`Last Update` = {if("Last_Update" %in% names(.)) Last_Update else `Last Update`}) %>%
-  rename(`Date`=`Last Update`) %>%
   mutate(`Province/State` = ifelse(grepl(",", `Province/State`), gsub(", ", ",",`Province/State`), paste0("City,",`Province/State`))) %>%
   separate(`Province/State`, into = c("City", "Province/State"), sep=",") %>%
   mutate(`Province/State` = ifelse(`Province/State`=="", `Country/Region`, `Province/State`)) %>%
